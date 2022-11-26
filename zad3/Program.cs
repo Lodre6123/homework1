f0b2c7cb30a21e9592541bbb2301fae9cfe6940c
Lodre6123 <nikpash6123@mail.ru>
2022-11-26 12:02:49 +0300

    Сделали 3 задание

---
+++ zad3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0)
+{
+    Console.WriteLine($"число {number} четное");
+}
+else
+{
+    Console.WriteLine($"число {number} не четное");
+}
